a2b9c31264e8be93e23f14211912a51e243a5f45
Jere <jere.chiosso@gmail.com>
2020-02-27 18:31:08 -0300

    Commit inicial, hola mundo

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'sass-loader', 'css-loader' ]
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'docs'),
+        historyApiFallback: true,
+        port: 3000
+    }
+}
